86f78e8b7e2aedcff814f0c2ee38d2105e81011d
Paweł Bartkiewicz <122686426+Hexyr7@users.noreply.github.com>
2026-03-02 17:50:31 +0100

    Add files via upload

---
+++ software/learning/learning_9/BlueScene.cpp
@@ -0,0 +1,64 @@
+// Author:  Paweł Bartkiewicz 
+// email:   15g.pawel.bart@gmail.com
+// github:  Hexyr7
+// licence: MIT
+
+#include "BlueScene.h"
+#include <TFT_eSPI.h>
+
+extern TFT_eSPI tft;
+//extern Buttons buttons;
+
+void BlueScene::setRedScene(Scene* scene) {
+  redScene = scene;
+}
+void BlueScene::setYellowScene(Scene* scene) {
+  yellowScene = scene;
+}
+void BlueScene::setGreenScene(Scene* scene) {
+  greenScene = scene;
+}
+void BlueScene::setBlinkScene(Scene* scene) {
+  blinkScene = scene;
+}
+void BlueScene::onEnter() {
+  Serial.println("BlueScene");
+  tft.fillScreen(TFT_BLUE);
+}
+
+void BlueScene::onExit() {}
+
+void BlueScene::update() {
+  //message = ""; 
+  //message = Serial.readString();
+  while(Serial.available() > 0) {
+    char c = Serial.read();
+    if (c =='\r' || c == '\n') {
+      message.trim();
+      Serial.println("Received: " + message);
+      message = "";
+    } else {
+      message += c;
+    }
+  }
+  
+  if (context.buttons.isClicked()) {
+    nextScene = blinkScene;
+  }
+  //if (Serial.available() > 0) {
+
+  
+    //Serial.println("Serial available");
+    if (message.startsWith("red")) {
+      Serial.println("red");
+      nextScene = redScene;
+    }
+    if (message.startsWith("yellow")) {
+      nextScene = yellowScene;
+    }
+    if (message.startsWith("green")) {
+      nextScene = greenScene;
+    }
+  //}
+  //Serial.println("BLUE update");
+}
