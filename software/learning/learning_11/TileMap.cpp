49cdca1b90d3695a352fb8bf9c48da14fa6251f4
Paweł Bartkiewicz <122686426+Hexyr7@users.noreply.github.com>
2026-04-12 17:41:26 +0200

    Add files via upload

--- software/learning/learning_11/TileMap.cpp
+++ software/learning/learning_11/TileMap.cpp
@@ -12,8 +12,8 @@ TileMap::TileMap(Context& ctx)
 void TileMap::draw() {
   auto& gph = context.renderer;
 
-  for (int x = 0; x < WIDTH; x++) {
-    for (int y = 0; y < HEIGHT; y++ ) {
+  for (int y = 0; y < HEIGHT; y++ ) {
+    for (int x = 0; x < WIDTH; x++) {
 
       int tile = themap[x][y];
 
@@ -21,7 +21,7 @@ void TileMap::draw() {
       int screenY = y * TILE_SIZE;
 
       if (tile == 1) {
-        gph.fillRect(screenX, screenY, TILE_SIZE, TILE_SIZE, TFT_PINK);
+        gph.fillRect(screenX, screenY, TILE_SIZE, TILE_SIZE, TFT_YELLOW);
       } else {
         gph.fillRect(screenX, screenY, TILE_SIZE, TILE_SIZE, TFT_BLACK);  
       }
